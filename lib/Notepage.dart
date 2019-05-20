1d27c2d9789dd881321bff0d89fe166d1e8e0499
ari muhamad ramdhan <50137246+aricangcimen@users.noreply.github.com>
2019-05-21 03:37:15 +0700

    Update Notepage.dart

--- lib/Notepage.dart
+++ lib/Notepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:BAGIAN_FLUTTER/DBHelper.dart';
-import 'package:BAGIAN_FLUTTER/mynote.dart';
+import 'package:bagian_flutter/DBHelper.dart';
+import 'package:bagian_flutter/mynote.dart';
 
 
 class Notepage  extends StatefulWidget {
@@ -24,8 +24,54 @@ class _Notepage State extends State<Notepage > {
    
    
     return scaffold(
-      appBar:  AppBar(title: Text(title , style: TextStyle(color: Colors.white , fontSize = 20.0)) )
-      
+      appBar:  AppBar(title: Text(
+        title : Center
+        child : Text (title , style: TextStyle(color: Colors.white , fontSize = 20.0),) ,
+        backgroundColor : Colors.white,             
+                     elevation : 0.0 ,
+                      action : <Widget>
+                       IconButton(
+                         onPressed : () => Navigator.pop(contex),
+                         icon : Icon(Icons.close , color : Colors.black , size : 25.0 ,),
+                    )
+                       ),
+      ),
+      backgroundColor : Colors.white ,
+      body : column(
+        children : <Widget>[
+          Row (
+            children: <Widget>
+            [ Createbutton(icon : Icon.save , enable :
+            
+      ],
+          ),
+        ],
+        ),
+    );
+  }
+}
+class CreateButton extends StatelessWidget {
+  final IconData icon;
+  final bool enable;
+  final onpress;
+  CreateButton(this.icon , this.enable ,this.onpress)
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(shape: BoxShape.circle , color: enable? Colors.purple: Colors.grey),
+      child: IconButton(
+icon: Icon(icon),
+color: Colors.white,
+iconSize: 18.0,
+onPressed: (){
+if(enable){
+  onpress();
+}
+
+},
+
+      ),
     );
   }
 }
